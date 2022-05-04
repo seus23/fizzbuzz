b0e5c6ec1fc9264346316dc51f026006693bed21
selene <seleneugs@gmail.com>
2022-05-04 15:37:31 -0500

    Ejecutando linter para correccion de cógido

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,19 +1,19 @@
-const ExplorerController = require('./../../lib/controllers/ExplorerController');
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Unit Tests for ExplorerController Class", () => {
     test("1) Cuales explorers estan en node", () => {
-        const cualesExplorers = ExplorerController.getExplorersByMission('node');
+        const cualesExplorers = ExplorerController.getExplorersByMission("node");
         const howmanyExp = cualesExplorers.length;
         expect(howmanyExp).toBe(10);
     });
 
     test("2) Username de explorer", () => {
-        const expUsernames = ExplorerController.getExplorersUsernamesByMission('node');
+        const expUsernames = ExplorerController.getExplorersUsernamesByMission("node");
         expect(expUsernames).toContain("ajolonauta1");
     });
 
     test("3) Cunatos explorers en java", () => {
-        const hmExplorers = ExplorerController.getExplorersAmonutByMission('java');
+        const hmExplorers = ExplorerController.getExplorersAmonutByMission("java");
         expect(hmExplorers).toBe(5);
     });
 
